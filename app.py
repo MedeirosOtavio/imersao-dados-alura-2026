819e84762c31de12bccfc74f42390fa54a33eec4
MedeirosOtavio <51581289+MedeirosOtavio@users.noreply.github.com>
2026-02-03 11:22:17 -0300

    Update app.py
    Adicionando texto no título.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
 st.set_page_config(
-    page_title="Dashboard de Salários na Área de Dados",
+    page_title="Dashboard de Salários na Área de Dados no mundo",
     page_icon="📊",
     layout="wide",
 )
